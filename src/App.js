eb6111e72374da38550d11231222210e0ccefaf3
ntpoom9 <80139353+ntpoom9@users.noreply.github.com>
2021-08-24 17:09:54 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
+import AppHeader from './components/AppHeader';
+import Caritem from './components/Caritem';
+import cars from './data/data';
+import CarPost from './components/CarPost';
+import AppSearch from './components/AppSearch'
+
+
 
 function App() {
+  const [seletedCar,setSeletedCar] = useState(null);
+  const [searchText, setSearchText] = useState('');
+
+    function onCarOpenClick(car) {
+    setSeletedCar(car);
+    }
+
+    function onCarCloseClick() {
+      setSeletedCar(null);
+    }
+
+    const carItems = cars.filter((car) => {
+      return car.title.includes(searchText);
+    }).map((car,index)=>{
+    return <Caritem key={index} car={car} onCarClick={onCarOpenClick}/>;
+  });
+
+
+    let carPost= null;
+    if(!!seletedCar){
+      carPost =<CarPost car={seletedCar} onBgclick={onCarCloseClick}/>
+    }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppHeader/>
+      <br/>
+
+      <div className="app-container"> 
+        <AppSearch value={searchText} onValueChange={setSearchText} />
+        <div className="app-grid">
+        
+            {carItems}
+        
+        </div>
+      </div>
+      {carPost}
     </div>
   );
 }
